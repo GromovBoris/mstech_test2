3211d94abd235413a919f19607a8290aae2feb08
GromovBoris <drunkgonzo@gmail.com>
2025-04-29 16:59:00 +0300

    feat: function transparent 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,101 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const overlay = document.getElementById("overlay");
+//   const input = document.getElementById("text");
+//   const submit = document.getElementById("submit");
+//   const enter = document.getElementById("enter");
+
+//   //   function generate(text) {
+//   //     const words = text.split(" ");
+//   //     overlay.innerHTML = "";
+//   //     // alert(words.length);
+
+//   //     let line = document.createElement("div");
+//   //     line.className = "line";
+//   //     overlay.appendChild(line);
+
+//   //     const overlayWidth = overlay.offsetWidth;
+
+//   //     let lastText = null;
+//   //     let lastSpace = null;
+
+//   //     for (let i = 0; i < words.length; i++) {
+//   //       const spanText = document.createElement("span");
+//   //       spanText.className = "text";
+//   //       spanText.textContent = words[i] + " ";
+
+//   //       const spanSpace = document.createElement("span");
+//   //       spanSpace.className = "space";
+//   //       spanSpace.textContent = " ";
+
+//   //       if (
+//   //         line.offsetWidth + spanText.offsetWidth + spanSpace.offsetWidth >=
+//   //         overlayWidth
+//   //       ) {
+//   //         if (lastText) {
+//   //           line.removeChild(lastText);
+//   //           if (lastSpace) {
+//   //             line.removeChild(lastSpace);
+//   //           }
+//   //           lastText.textContent = lastText.textContent.trimEnd();
+//   //         }
+//   //         let line = document.createElement("div");
+//   //         line.className = "line";
+//   //         overlay.appendChild(line);
+//   //         // continue;
+//   //       } else {
+//   //         line.appendChild(spanText);
+//   //         line.appendChild(spanSpace);
+
+//   //         lastText = spanText;
+//   //         lastSpace = spanSpace;
+//   //       }
+
+//   //   if (i < words.length - 1) {
+//   //     wordSpan.textContent += " ";
+//   //   }
+
+//   //   if (i < words.length - 1) {
+//   //     const spacer = document.createElement("span");
+//   //     spacer.className = "space";
+//   //     overlay.appendChild(spacer);
+//   //   }
+//   // }
+//   //   }
+
+//   function generate(text) {
+//     const words = text.split(" ");
+//     overlay.innerHTML = "";
+//     // alert(words.length);
+
+//     let line = (document.createElement("div").className = "line");
+
+//     for (let i = 0; i < words.length; i++) {
+//       const wordSpan = document.createElement("span");
+//       wordSpan.className = "text";
+//       wordSpan.textContent = words[i];
+//       overlay.appendChild(wordSpan);
+
+//       if (i < words.length - 1) {
+//         wordSpan.textContent += " ";
+//       }
+
+//       if (i < words.length - 1) {
+//         const spacer = document.createElement("span");
+//         spacer.className = "space";
+//         overlay.appendChild(spacer);
+//       }
+//     }
+//   }
+
+//   submit.addEventListener("click", function () {
+//     generate(input.value);
+//     enter.remove();
+//   });
+
+//   //   window.addEventListener("resize", generateTextElements(input.value));
+// });
+//
+
 document.addEventListener("DOMContentLoaded", function () {
   const overlay = document.getElementById("overlay");
   const input = document.getElementById("text");
@@ -29,9 +127,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   //   input.addEventListener("input", transparent());
 
-  window.addEventListener("resize", transparent);
+  // window.addEventListener("resize", transparent);
 });
 
 // Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex provident
-//         corporis libero veniam exercitationem perferendis ducimus voluptatibus
-//         possimus modi nostrum.
+// corporis libero veniam exercitationem perferendis ducimus voluptatibus
+// possimus modi nostrum.
